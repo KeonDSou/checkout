4c11541b48504345053e941f3cde0f205b1bb13c
KeonDSou <keoni.dsouza@itv.com>
2019-02-27 13:57:33 +0000

    CK-0003 - WIP for calculateTotal function

--- src/main/scala/Checkout.scala
+++ src/main/scala/Checkout.scala
@@ -92,37 +92,95 @@ object Checkout extends App {
     * @param items Products to be purchased
     * @return Item subtotal
     */
+  //  def calculateSubtotalWithFold(items: List[Item]): Int =
+  //    items
+  //      .map(_.price)
+  //      .sum
+  //
   def calculateSubtotalWithFold(items: List[Item]): Int =
-    items
-      .map(_.price)
-      .sum
+    items.foldLeft(0) {
+      case (acc, item) => acc + item.price
+    }
 
   // TODO Work out what's happening from here (it's not been sorted yet)
 
   /**
     * Calculate total price of shop
     *
-    * @param items  Products to be purchased
+    * @param items Products to be purchased
     * @return Total price of shop
     */
-  def calculateTotal(items: List[Item]) =
-    items
-      .groupBy(_.sku)
-      .mapValues(_.size)
+  def calculateTotal(items: List[Item]) = {
+    println("calculateTotal")
+  }
+}
+//    items.filter(p: Item => Some(p.offer))
+//    }
+//    items.partition(p: Inventory.Item => Some(p.offer))
+//    p: Inventory.Item => Boolean
+//    val groupedByOffer =
+//      items
+//        .groupBy(_.offer)
+//        .map(println)
+////        val gbo = items.contains(Item.offer)
+//    groupedByOffer.foreach(println)
+//    //    println("items:                                 " + items)
+//    println(".groupBy(_.sku):                       " + items.groupBy(_.sku))
+//    println(
+//      ".mapValues(_.size):                    " + items
+//        .groupBy(_.sku)
+//        .mapValues(_.size))
 
-  println(calculateTotal(List(itemC, itemC, itemD)))
-//      items.flatMap(_.price)
+// How many of each item are there?
+//    val skus: Map[String, List[Item]] = items.groupBy(_.sku)
+//    println("skus -> " + skus)
 //
-//      items // Dave's (better) version
-//        .map(t => (t._1, t._2.length))
-//        .map {
-//          case (item, quantity) if offers.contains(item) =>
-//            calculateItemTotal(item, quantity, offers(item))
-//
-//          case (item, quantity) =>
-//            calculateItemTotal(item, quantity)
+//    def calculateItemTotal(skus: Map[String, List[Item]], acc: Int) = {
+//      // For each item in the map...
+//      skus(sku)
+//      println("skus(acc) => " + skus("A"))
+//      // Length of item list
+//      // Is it on offer?
+//      // Calculate price accordingly
+//    }
+//    calculateItemTotal(skus, 0)
+//  }
+//  calculateTotal(testShop)
+// Use the offer option to work out the total item price
+// Add the total item prices together (grand total)
+
+//    val (promoItems: List[Inventory.Item],
+//    nonPromoItems: List[Inventory.Item]) = items.collect({
+//      case Item(sku, price, offer) =>
+//        offer match {
+//          case Some(offer: Offer) => (sku, price, offer)
+//          case None => (sku, price)
 //        }
-//        .sum
+//    })
+//}}
+//    println("itemsOnOffer -> " + itemsOnOffer)
+
+//    val products: Map[String, List[Inventory.Item]] = items.groupBy(_.sku)
+////    products.collect({case (sku:String, price:Int, offer: Offer) => offer})
+//    products.collect({ case (sku: String, item: Inventory.Item) => sku })
+////      .mapValues(_.size)
+////    friends collect { case (id, p) if (p.age > 22) => p}
+
+//  println(
+//    "total: " + calculateTotal(List(itemC, itemB, itemC, itemD, itemB, itemA)))
+//    items.flatMap(_.price)
+
+//items // Dave's (better) version
+//    .map(t => (t._1, t._2.length))
+//    .map {
+//      case (item, quantity) if offers.contains(item) =>
+//        calculateItemTotal(item, quantity, offers(item))
+//
+//      case (item, quantity) =>
+//        calculateItemTotal(item, quantity)
+//    }
+//    .sum
+//  }
 
 //    /**
 //      * Calculate total price of n item(s) on offer
@@ -158,4 +216,4 @@ object Checkout extends App {
 //          promo + fullPrice
 //      }
 //    }
-}
+//}
